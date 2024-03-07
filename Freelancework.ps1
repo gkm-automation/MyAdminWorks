2b172c68f1bdab7cdc9125196a3134d4c7eef44a
gkm-automation <52336739+gkm-automation@users.noreply.github.com>
2024-03-07 15:45:45 +0530

    Update Freelancework.ps1

--- Freelancework.ps1
+++ Freelancework.ps1
@@ -1,14 +1,23 @@
 #Change Baseline CSV path
-$base = Import-Csv "C:\Users\kg212483\Downloads\results\base.csv" | Where-Object { $_.label -Like "ClaimsFNOL*" } | Select-Object label,@{n='elapsed';e={$_.elapsed/1000}}
+$base = Import-Csv "C:\Users\\Downloads\results\base.csv" | Where-Object { $_.label -Like "ClaimsFNOL*" } | Select-Object label,@{n='elapsed';e={$_.elapsed/1000}}
 #Change Current CSV Path
-$current = Import-Csv "C:\Users\kg212483\Downloads\results\current.csv" | Where-Object { $_.label -Like "ClaimsFNOL*" } | Select-Object label,@{n='elapsed';e={$_.elapsed/1000}}
+$current = Import-Csv "C:\Users\\Downloads\results\current.csv" | Where-Object { $_.label -Like "ClaimsFNOL*" } | Select-Object label,@{n='elapsed';e={$_.elapsed/1000}}
 $results = @()
 foreach($record in $current){
     $basevalue = ($base | Where-Object { $_.label -eq $record.label }).'elapsed'[0]
     $diffValue = $basevalue - $($record.'elapsed')
+    $divMark = (($basevalue/100)*5)+ $basevalue
+    If($($record.'elapsed') -ge $divMark){
+        $DeviationThreshold = "Reached"
+    }
+    Else{
+        $DeviationThreshold = "Below Limit"
+    }
     $obj = [PSCustomObject]@{
         Label = $record.label
-        'Difference(Secs)' = $diffValue
+       'Difference(Secs)' = $diffValue
+       'DeviationThreshold' = $DeviationThreshold
+
     }
 $results += $obj
 }
